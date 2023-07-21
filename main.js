f5ccdb26dc40f80f9b9ab7a2defe91abef7cae7a
theroyalcoder <anusanth94@gmail.com>
2023-07-21 10:32:51 +0200

    Finished first Beta

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ window.addEventListener('load', () => {
 
         const task_content_el = document.createElement("div");
         task_content_el.classList.add("content");
+        // task_content_el.innerText = task;
 
         task_el.appendChild(task_content_el);
         
@@ -34,10 +35,34 @@ window.addEventListener('load', () => {
 
         const task_edit_el = document.createElement("button");
         task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML = "Edit";
 
-        
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML = "Delete";
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
 
         list_el.appendChild(task_el);
-    })
 
-})
+        input.value = "";
+
+        task_edit_el.addEventListener('click', () => {
+            if(task_edit_el.innerText.toLowerCase() == "edit") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Edit";
+            }
+        });
+
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+        });
+    });
+});
